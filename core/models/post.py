23f86beffc940ad1f2f06fb953a3d66aa44a364a
Yura Frits <twinkreferal22847228@gmail.com>
2025-09-11 22:27:39 +0300

    create User relationship Mixin class

--- core/models/post.py
+++ core/models/post.py
@@ -3,18 +3,21 @@ from sqlalchemy import String, Text, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.models import Base
+from .mixins import UserRelationMixin
 
 if TYPE_CHECKING:
     from core.models import User
 
 
-class Post(Base):
+class Post(UserRelationMixin, Base):
+
+    # _user_id_nullable = False
+    # _user_id_unique = False
+    _user_back_populates = "posts"
+
     title: Mapped[str] = mapped_column(String(55), unique=True)
     body: Mapped[str] = mapped_column(
         Text,
         default="",
         server_default="",
     )
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-
-    user: Mapped["User"] = relationship(back_populates="posts")
--- core/models/profile.py
+++ core/models/profile.py
@@ -5,10 +5,10 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .base import Base
 
 if TYPE_CHECKING:
-    from .post import Post
+    from .user import User
 
 
-class User(Base):
+class Profile(Base):
     username: Mapped[str] = mapped_column(String(25), unique=True)
 
-    posts: Mapped[list["Post"]] = relationship(back_populates="user")
+    user_id: Mapped["User"] = relationship(back_populates="user")
